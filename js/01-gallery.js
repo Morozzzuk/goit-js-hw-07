f6a021f1cc6b71db0d211524f02979b0eca02450
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-03-14 00:30:54 +0200

    add answer task 01-gallery.html and js
    add .gitignore
    add answer task 01-gallery.html and js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//* Add markup for gallery
+
+const galleryEl = document.querySelector('.gallery');
+// console.log(galleryEl);
+const makeGalleryMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt="${description}"
+    />
+  </a>
+</div>`;
+})
+    .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+galleryEl.addEventListener('click', imgOpenModal);
+
+//* Add modal window for gallery
+
+function imgOpenModal(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const currentImage = e.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${currentImage}"width="700" height="500">`);
+    instance.show();
+
+    //* add Escape to the gallery
+
+    window.addEventListener('keydown', pressEscapeKeyboard);
+    function pressEscapeKeyboard(e) {
+        if (e.code === 'Escape') {
+            instance.close();
+            window.removeEventListener('keydown', pressEscapeKeyboard);
+        }
+    }
+}
